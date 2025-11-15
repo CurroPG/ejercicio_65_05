2ad1907a8b3ae5babc1b046e5910f49763c2e474
Curro Portillo <211curroportillo@gmail.com>
2025-11-15 18:02:36 +0100

    ejercicio 65

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de la A (mayor o igual que 3): "));
+        int palito = Integer.parseInt(System.console().readLine("Introduzca la fila del palito horizontal (mayor o igual que 2): "));
+        int espacios = altura - 1;
+        int base = 1;
+        int fila = 1;
+
+        while(base <= altura){
+            for(int i = 0; i < espacios; i++){
+                System.out.print(" ");
+            }
+            for(int i = 1; i <= fila; i++){
+                if(base == palito)
+                    System.out.print("*");
+                    else if(i == 1 || i == fila)
+                        System.out.print("*");
+                        else
+                            System.out.print(" ");
+            }
+            System.out.println();
+            base++;
+            fila+=2;
+            espacios--;
+        }
+    
     }
 }
